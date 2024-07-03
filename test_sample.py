3854462cdd820146be64d7dfed21d08f2d0d42f6
JPumpkin1223 <jpark9595@naver.com>
2024-07-04 03:04:07 +0900

    Feat: Implement multiplication and equality tests - Added tests for multiplication and equality to ensure correct functionality.

--- test_sample.py
+++ test_sample.py
@@ -3,13 +3,11 @@ from models import Dollor
 
 def test_multiplication() -> None:
     five: Dollor = Dollor(5)
-    # product: Dollor = five.times(2)
     assert Dollor(10) == five.times(2)
-    # product = five.times(3)
     assert Dollor(15) == five.times(3)
 
 
 def test_equality() -> None:
     five: Dollor = Dollor(5)
     assert five == Dollor(5)
-    assert five != Dollor(6)
+    assert not five == Dollor(6)
