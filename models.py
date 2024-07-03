62eb99e2aad6092bb449ebea5a770968727a239e
JPumpkin1223 <jpark9595@naver.com>
2024-07-04 03:01:20 +0900

    Feat: Implement custom equality method in Dollor class - Add custom __eq__ method to compare Dollor instances for equality.

--- models.py
+++ models.py
@@ -2,8 +2,10 @@ class Dollor:
     def __init__(self, amount: int):
         self.amount = amount
 
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Dollor):
+            return False
+        return self.amount == other.amount
+
     def times(self, multiplier: int) -> "Dollor":
         return Dollor(self.amount * multiplier)
-
-    def equals(self, other: "Dollor") -> bool:
-        return self.amount == other.amount
--- test_sample.py
+++ test_sample.py
@@ -3,13 +3,13 @@ from models import Dollor
 
 def test_multiplication() -> None:
     five: Dollor = Dollor(5)
-    product: Dollor = five.times(2)
-    assert product.amount == 10
-    product = five.times(3)
-    assert product.amount == 15
+    # product: Dollor = five.times(2)
+    assert Dollor(10) == five.times(2)
+    # product = five.times(3)
+    assert Dollor(15) == five.times(3)
 
 
 def test_equality() -> None:
     five: Dollor = Dollor(5)
-    assert five.equals(Dollor(5))
-    assert not five.equals(Dollor(6))
+    assert five == Dollor(5)
+    assert five != Dollor(6)
