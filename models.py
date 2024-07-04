99d7cb101f1004fe75d00d600d210435abf92ebc
JPumpkin1223 <jpark9595@naver.com>
2024-07-04 16:45:39 +0900

    Feat: Add class method for creating Dollar objects - Introduces a new class method to create Dollar objects easily within the Money class. Enhances code readability and promotes code reusability.

--- models.py
+++ models.py
@@ -1,6 +1,13 @@
+# from abc import ABCMeta, abstractmethod
+
+
 class Money:
     def __init__(self, amount: int):
         self._amount = amount
+
+    @classmethod
+    def dollar(cls, amount: int) -> "Dollar":
+        return Dollar(amount)
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, Money):
@@ -9,11 +16,11 @@ class Money:
         return self._amount == other._amount and type(self) == type(other)
 
 
-class Dollor(Money):
-    def times(self, multiplier: int) -> "Dollor":
-        return Dollor(self._amount * multiplier)
+class Dollar(Money):
+    def times(self, multiplier: int) -> Money:
+        return Dollar(self._amount * multiplier)
 
 
 class Franc(Money):
-    def times(self, multiplier: int) -> "Franc":
+    def times(self, multiplier: int) -> Money:
         return Franc(self._amount * multiplier)
--- test_sample.py
+++ test_sample.py
@@ -1,10 +1,10 @@
-from models import Dollor, Franc, Money
+from models import Dollar, Franc, Money
 
 
 def test_dollor_multiplication() -> None:
-    five: Dollor = Dollor(5)
-    assert Dollor(10) == five.times(2)
-    assert Dollor(15) == five.times(3)
+    five: Dollar = Money.dollar(5)
+    assert Dollar(10) == five.times(2)
+    assert Dollar(15) == five.times(3)
 
 
 def test_franc_multiplication() -> None:
@@ -14,8 +14,8 @@ def test_franc_multiplication() -> None:
 
 
 def test_equality() -> None:
-    assert Dollor(5) == Dollor(5)
-    assert not Dollor(5) == Dollor(6)
+    assert Dollar(5) == Dollar(5)
+    assert not Dollar(5) == Dollar(6)
     assert Franc(5) == Franc(5)
     assert not Franc(5) == Franc(6)
-    assert not Dollor(5) == Franc(5)
+    assert not Dollar(5) == Franc(5)
