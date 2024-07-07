eef22e2b0498e43422d3ec960754f82a50a38729
JPumpkin1223 <jpark9595@naver.com>
2024-07-07 22:32:53 +0900

    Feat: Implement new functionality for reducing money amounts - Updated the expression class to include a method for reducing money values - Added a rate calculation method in the Bank class - Included new test cases for verifying currency conversion rates

--- models.py
+++ models.py
@@ -1,5 +1,10 @@
-class Expression:
-    pass
+from abc import ABCMeta, abstractmethod
+
+
+class Expression(metaclass=ABCMeta):
+    @abstractmethod
+    def reduce(self, bank: "Bank", target: str) -> "Money":
+        pass
 
 
 class Money(Expression):
@@ -18,6 +23,10 @@ class Money(Expression):
     def plus(self, addend: "Money") -> "Expression":
         return Sum(self.amount + addend.amount, self.currency)
 
+    def reduce(self, bank: "Bank", target: str) -> "Money":
+        rate: bank = bank.rate(self.currency, target)
+        return Money(self.amount / rate, target)
+
     @classmethod
     def dollar(cls, amount: int) -> "Money":
         return Money(amount, "USD")
@@ -28,11 +37,22 @@ class Money(Expression):
 
 
 class Bank:
-    def reduce(self, source: Expression, to: str) -> Money:
-        if isinstance(source, Money):
-            return source
-        sum: Sum = source
-        return sum.reduce(to)
+    def __init__(self) -> None:
+        self.__rates: dict = {}
+
+    def reduce(self, source: Expression, target: str) -> Money:
+        return source.reduce(self, target)
+
+    def rate(self, origin: str, target: str) -> int:
+        if origin == target:
+            return 1
+        rate: int | None = self.__rates.get(Pair(origin, target))
+        if rate is not None:
+            return rate
+        return 1
+
+    def add_rate(self, origin: str, target: str, rate: int) -> None:
+        self.__rates[Pair(origin, target)] = rate
 
 
 class Sum(Expression):
@@ -40,6 +60,20 @@ class Sum(Expression):
         self.augend = augend
         self.addend = addend
 
-    def reduce(self, to: str) -> Money:
+    def reduce(self, bank: "Bank", target: str) -> Money:
         amount = self.augend.amount + self.addend.amount
-        return Money(amount, to)
+        return Money(amount, target)
+
+
+class Pair:
+    def __init__(self, origin: str, target: str):
+        self.__origin = origin
+        self.__target = target
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Pair):
+            return False
+        return self.__origin == other.__origin and self.__target == other.__target
+
+    def __hash__(self) -> int:
+        return hash((self.__origin, self.__target))
