713a02371c61e3a289a6a1f90d1a99c58c47dbdd
JPumpkin1223 <jpark9595@naver.com>
2024-07-12 17:33:09 +0900

    Fix: Update return type of a method to "Money" for clarity and accuracy. - Ensure consistency and understanding when dealing with financial expressions.

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from abc import ABCMeta, abstractmethod
 
 class Expression(metaclass=ABCMeta):
     @abstractmethod
-    def reduce(self, bank: "Bank", target: str) -> "Expression":
+    def reduce(self, bank: "Bank", target: str) -> "Money":
         pass
 
     @abstractmethod
