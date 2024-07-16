8109d16e22bd438aad9aa036d4e944bf4e7639f8
JPumpkin1223 <jpark9595@naver.com>
2024-07-16 23:46:13 +0900

    Fix: Removed unnecessary commented code related to setup in a test method.
    - Enhances code readability by eliminating redundant and obsolete setup logic.

--- chapter_2_xunit/test_chapter_2.py
+++ chapter_2_xunit/test_chapter_2.py
@@ -2,9 +2,6 @@ from xunit_models import WasRun
 
 
 class TestCaseTest:
-    # def setup_method(self, _) -> None:
-    #     self.test = WasRun("test_method")
-
     def test_template_method(self) -> None:
         self.test = WasRun("test_method")
         self.test.run()
