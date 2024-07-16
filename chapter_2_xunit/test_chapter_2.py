1128fc0cf9f440e7d0f223e702cee7297afc9813
JPumpkin1223 <jpark9595@naver.com>
2024-07-17 00:00:10 +0900

    Fix: Update variable usage in test method for consistency and readability
    - Replace self.test with a local variable for consistency and clarity in the test method.

--- chapter_2_xunit/test_chapter_2.py
+++ chapter_2_xunit/test_chapter_2.py
@@ -3,6 +3,6 @@ from xunit_models import WasRun
 
 class TestCaseTest:
     def test_template_method(self) -> None:
-        self.test = WasRun("test_method")
-        self.test.run()
-        assert "setUp testMethod tearDown " == self.test.log
+        test = WasRun("test_method")
+        test.run()
+        assert "setUp testMethod tearDown " == test.log
