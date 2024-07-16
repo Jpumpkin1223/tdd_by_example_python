5f707d3d9833f20837a2e4f43587d310904a09d2
JPumpkin1223 <jpark9595@naver.com>
2024-07-16 18:39:52 +0900

    Feat: Add a new test method - Introduce a new test method to enhance testing capabilities for the application.

--- chapter_2_xunit/test_chapter_2.py
+++ chapter_2_xunit/test_chapter_2.py
@@ -1,11 +1,14 @@
 from xunit_models import WasRun
 
 
-def test_running():
+def test_running() -> None:
     test = WasRun("test_method")
     assert not test.was_run
     test.run()
     assert test.was_run
 
 
-# def test
+def test_set_up() -> None:
+    test = WasRun("test_method")
+    test.run()
+    assert test.was_set_up
--- chapter_2_xunit/xunit_models.py
+++ chapter_2_xunit/xunit_models.py
@@ -3,14 +3,22 @@ class TestCase:
         self.name = name
 
     def run(self) -> None:
+        self.set_up()
         method = getattr(self, self.name)
         method()
+
+    def set_up(self) -> None:
+        pass
 
 
 class WasRun(TestCase):
     def __init__(self, name: str) -> None:
         self.was_run = False
+        self.was_set_up = False
         super().__init__(name)
 
     def test_method(self) -> None:
         self.was_run = True
+
+    def set_up(self) -> None:
+        self.was_set_up = True
