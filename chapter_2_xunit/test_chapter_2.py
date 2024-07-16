76472b24b23b3a27178a76462f933d231fda649f
JPumpkin1223 <jpark9595@naver.com>
2024-07-16 23:46:00 +0900

    Feat: Add logging functionality to WasRun class - Modify WasRun class to include logging of test execution steps.

--- chapter_2_xunit/test_chapter_2.py
+++ chapter_2_xunit/test_chapter_2.py
@@ -2,13 +2,10 @@ from xunit_models import WasRun
 
 
 class TestCaseTest:
-    def setup_method(self, _) -> None:
+    # def setup_method(self, _) -> None:
+    #     self.test = WasRun("test_method")
+
+    def test_template_method(self) -> None:
         self.test = WasRun("test_method")
-
-    def test_running(self) -> None:
         self.test.run()
-        assert self.test.was_run
-
-    def test_set_up(self) -> None:
-        self.test.set_up()
-        assert self.test.was_set_up
+        assert "setUp testMethod " == self.test.log
--- chapter_2_xunit/xunit_models.py
+++ chapter_2_xunit/xunit_models.py
@@ -14,11 +14,12 @@ class TestCase:
 class WasRun(TestCase):
     def __init__(self, name: str) -> None:
         self.was_run = False
-        self.was_set_up = False
+        self.log = ""
         super().__init__(name)
 
     def test_method(self) -> None:
         self.was_run = True
+        self.log += "testMethod "
 
     def set_up(self) -> None:
-        self.was_set_up = True
+        self.log = "setUp "
