cb333ad43d46d4bd1c19b7d36a3cefdf36000254
JPumpkin1223 <jpark9595@naver.com>
2024-07-17 01:01:11 +0900

    Fix: Add TestResult object to record test results
    - Add TestResult object to track and summarize test outcomes in test methods.

--- chapter_2_xunit/test_chapter_2.py
+++ chapter_2_xunit/test_chapter_2.py
@@ -4,17 +4,20 @@ from xunit_models import TestResult, TestSuite, WasRun
 class TestCaseTest:
     def test_template_method(self) -> None:
         test = WasRun("test_method")
-        test.run()
+        result = TestResult()
+        test.run(result)
         assert "setUp testMethod tearDown " == test.log
 
     def test_result(self) -> None:
         test = WasRun("test_method")
-        result = test.run()
+        result = TestResult()
+        test.run(result)
         assert "1 run, 0 failed" == result.summary()
 
     def test_failed_result(self) -> None:
         test = WasRun("test_broken_method")
-        result = test.run()
+        result = TestResult()
+        test.run(result)
         assert "1 run, 1 failed" == result.summary()
 
     def test_failed_result_formatting(self) -> None:
